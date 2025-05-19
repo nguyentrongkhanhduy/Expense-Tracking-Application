bf7d9888dadfcf99977670de320db820c36286cb
duyntk2 <duyntk2@vng.com.vn>
2025-05-19 22:35:14 +0700

    Setup some server side code

--- Server/server.js
+++ Server/server.js
@@ -1,1 +1,23 @@
-//server
+const express = require("express");
+require("dotenv").config();
+
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+const port = process.env.PORT;
+// const databaseConnectString = process.env.MONGODB_URI;
+
+const startServer = async () => {
+  try {
+    console.log(`Server is running on http://localhost:${port}`);
+  } catch (error) {
+    console.log(error.message);
+  }
+};
+
+app.listen(port, startServer);
+
+app.get("/", (req, res) => {
+  res.send("Server is working!");
+});
