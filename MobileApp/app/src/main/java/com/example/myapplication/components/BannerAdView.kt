b8e43f82310151b716d7765880b01d1a8f5f57e6
nguyentrongkhanhduy <nguyentrongkhanhduy@gmail.com>
2025-06-10 12:15:57 +0700

    Update

--- MobileApp/app/src/main/java/com/example/myapplication/components/BannerAdView.kt
+++ MobileApp/app/src/main/java/com/example/myapplication/components/BannerAdView.kt
@@ -18,7 +18,7 @@ fun AdBanner(modifier: Modifier = Modifier) {
         factory = {
             AdView(context).apply {
                 setAdSize(AdSize.BANNER)
-                adUnitId = "ca-app-pub-5308421996916752/7686481922" // Key
+                adUnitId = "ca-app-pub-5308421996916752/7686481922" // Real Key
                 loadAd(AdRequest.Builder().build())
                 layoutParams = ViewGroup.LayoutParams(
                     ViewGroup.LayoutParams.MATCH_PARENT,
