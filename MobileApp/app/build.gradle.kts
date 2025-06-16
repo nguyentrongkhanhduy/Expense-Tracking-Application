ee84f288fe4d2f63cf87185f951a2b99a4179b59
davidHuyDang <davidhuydang02@gmail.com>
2025-06-16 12:38:16 -0400

    Revert "TEDJ-48 Update build version"

--- MobileApp/app/build.gradle.kts
+++ MobileApp/app/build.gradle.kts
@@ -22,13 +22,15 @@ android {
         applicationId = "com.example.myapplication"
         minSdk = 30
         targetSdk = 35
-        versionCode = project.findProperty("versionCode")?.toString()?.toInt() ?: 1
-        versionName = project.findProperty("versionName")?.toString() ?: "1.0.0"
+        versionCode = 1
+        versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         buildConfigField("String", "HF_API_TOKEN", "\"${localProperties["HF_API_TOKEN"] ?: ""}\"")
         buildConfigField("String", "CURRENCYLAYER_API_KEY", "\"${localProperties["CURRENCYLAYER_API_KEY"] ?: ""}\"")
 
+        versionCode = project.findProperty("versionCode")?.toString()?.toInt() ?: 1
+        versionName = project.findProperty("versionName")?.toString() ?: "1.0.0"
     }
 
     buildTypes {
