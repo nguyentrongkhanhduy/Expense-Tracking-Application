4f6cc1766b5d678049e17d8db4ca7a6bbe23f1dc
nguyentrongkhanhduy <nguyentrongkhanhduy@gmail.com>
2025-06-19 09:07:15 -0400

    Fix edit dialog formatted date

--- MobileApp/app/src/main/java/com/example/myapplication/screens/dialogs/EditTransactionDialog.kt
+++ MobileApp/app/src/main/java/com/example/myapplication/screens/dialogs/EditTransactionDialog.kt
@@ -90,7 +90,7 @@ fun EditTransactionDialog(
         timeZone = TimeZone.getTimeZone("UTC")
     }
     val formattedDate = utcFormat.format(
-        Date(datePickerState.selectedDateMillis ?: System.currentTimeMillis())
+        Date(viewModel.inputDate ?: System.currentTimeMillis())
     )
 
     // Location logic
